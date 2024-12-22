a7ee01c64e62891b57b68074b1e8d85b2f32dd54
Mayur Rastogi <mayur.rastogi.cseaiml.2022@miet.ac.in>
2024-12-22 18:48:20 +0530

    learn about animation
    scale animation
    translate animation
    rotate animation
    alpha animation

--- AnimationTypes/app/src/main/java/com/example/animationtypes/MainActivity.java
+++ AnimationTypes/app/src/main/java/com/example/animationtypes/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.animationtypes;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+        Animation moveAlpha,moveTranslate, moveRotate, moveScale;
+    Button btntranslate, btnalpha, btnrotate, btnscale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        moveAlpha = AnimationUtils.loadAnimation(this,R.anim.alphaa);
+        moveScale = AnimationUtils.loadAnimation(this,R.anim.scale);
+        moveRotate = AnimationUtils.loadAnimation(this,R.anim.rotate);
+        moveTranslate = AnimationUtils.loadAnimation(this,R.anim.translate);
+        text = findViewById(R.id.centerText);
+        btnalpha = findViewById(R.id.alpha);
+        btnrotate = findViewById(R.id.rotate);
+        btnscale = findViewById(R.id.scale);
+        btntranslate = findViewById(R.id.translate);
+
+        btnalpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.startAnimation(moveAlpha);
+            }
         });
+        btntranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.startAnimation(moveTranslate);
+            }
+        });
+        btnrotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.startAnimation(moveRotate);
+            }
+        });
+        btnscale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.startAnimation(moveScale);
+            }
+        });
+
     }
 }
